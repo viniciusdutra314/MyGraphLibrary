83d41e7c71dd7583542339f196180815e28fad53
Vinícius Dutra <dutravinisousa2@usp.br>
2025-09-19 19:26:38 +0000

    feat: reading edge list

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@ typedef struct {
     size_t to;
 } Edge;
 
+typedef double weight;
+
 int order_edges(const void *edge_a, const void *edge_b) {
     const Edge *first  = (const Edge*) edge_a;
     const Edge *second = (const Edge*) edge_b;
@@ -20,19 +22,40 @@ int order_edges(const void *edge_a, const void *edge_b) {
 }
 
 int main(){
-    Edge array[5]={{0,10},{3,4},{4,6},{6,1},{1,4}};
-    qsort(array, 5, sizeof(Edge), order_edges);
-
-    for (int i=0; i<5; i++){
-        printf("(%zu,%zu)\n", array[i].from, array[i].to);
+    FILE* file=fopen("../tests/floyd_warshall/test1.net","r");
+    if (file==NULL){
+        fprintf(stderr,"O arquivo não conseguiu ser aberto\n");
+        return 1;   
     }
-
-    Edge key = {4, 6};
-    Edge *found = bsearch(&key, array, 5, sizeof(Edge), order_edges);
-
-    if (found) {
-        printf("Found: (%zu,%zu)\n", found->from, found->to);
-    } else {
-        printf("Not found\n");
+    int V,E;
+    fscanf(file,"%d",&V);
+    fscanf(file,"%d",&E);
+    Edge* edge_list=malloc(E*sizeof(Edge));
+    weight* weight_array=malloc(E*sizeof(weight));
+    uint32_t num_line=0;
+    while (!feof(file) && num_line!=E){
+        Edge* current_edge=&edge_list[num_line];
+        weight* current_weight=&weight_array[num_line];
+        int characters_read=fscanf(file," %d %d %lf ",
+                                &current_edge->from,
+                                &current_edge->to,
+                             current_weight);
+        if (characters_read!=3){
+            fprintf(stderr,"Linha %d com formatação inválida");
+            return 1;
+        }                    
+        num_line++;
+    };
+    if (num_line!=E){
+        fprintf(stderr,"Número de edges incompatível com o arquivo fornecido\n");
+        return 1;
     }
+    for (int i=0;i<E;i++){
+        printf("%d %d %f \n",edge_list[i].from,
+        edge_list[i].to,
+        weight_array[i]);
+    }
+    free(weight_array);
+    free(edge_list);
+    fclose(file);
 }
